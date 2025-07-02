2dd48e5d20c615a00c6ca7db274654a7daa8d4b8
257lad <140239904+257lad@users.noreply.github.com>
2025-07-02 10:59:59 +0200

    commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import store from './store';
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
-axios.defaults.baseURL = 'http://192.168.1.67:8000/api/';
+// axios.defaults.baseURL = 'http://192.168.1.67:8000/api/';
 
 axios.interceptors.request.use(
   (config) => {
