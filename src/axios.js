6805fa88de3e64984c8af8c6bb32a46f6577e3f0
257lad <140239904+257lad@users.noreply.github.com>
2025-07-02 10:54:30 +0200

    commit

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import store from './store';
-// axios.defaults.baseURL = 'https://microfinance.amidev.bi/api/';
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
 axios.defaults.baseURL = 'http://192.168.1.67:8000/api/';
 
 axios.interceptors.request.use(
